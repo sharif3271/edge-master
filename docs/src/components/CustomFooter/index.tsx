351e8d193a300f4549ac87f339416696c2da022b
sharif eiri <sharif3271@gmail.com>
2023-09-07 16:58:14 +0300

    theme tiny changes

--- docs/src/components/CustomFooter/index.tsx
+++ docs/src/components/CustomFooter/index.tsx
@@ -9,7 +9,7 @@ import styles from "./styles.module.scss";
 const CustomFooter = () => (
 <footer style={{marginTop: 25}} className={styles["custom-footer-wrapper"]}>
   <div style={{display: 'flex'}} className={styles["logo-wrapper"]}>
-    <img src={useBaseUrl("/img/logo.svg")} /><span style={{transform: 'translateY(2px)', fontSize: 18, color: '#ea5906', marginLeft: 3}}><i><b> EdgeMaster</b></i></span>
+    <img src={useBaseUrl("/img/logo.svg")} /><span style={{transform: 'translateY(2px)', fontSize: 18, color: 'var(--ifm-color-primary-darker)', marginLeft: 3}}><i><b> EdgeMaster</b></i></span>
   </div>
   <div className={styles["copyright"]}>
     {`© ${new Date().getFullYear()} EdgeMaster. All rights reserved`}
@@ -17,7 +17,7 @@ const CustomFooter = () => (
   <div className={styles["footerSocialIconsWrapper"]}>
     <div className={styles["socialBrands"]}>
       <Link
-        href={"https://github.com/"}
+        href={"https://github.com/sharif3271/edge-master/"}
         rel="noopener noreferrer"
         aria-label={"Github"}
       >
@@ -26,16 +26,7 @@ const CustomFooter = () => (
     </div>
     <div className={styles["socialBrands"]}>
       <Link
-        href={"https://www.youtube.com/"}
-        rel="noopener noreferrer"
-        aria-label={"Youtube"}
-      >
-        <YoutubeIcon />
-      </Link>
-    </div>
-    <div className={styles["socialBrands"]}>
-      <Link
-        href={"https://www.linkedin.com/"}
+        href={"https://www.linkedin.com/in/sharif-eiri-15bb90a1/"}
         rel="noopener noreferrer"
         aria-label={"Linkedin"}
       >
